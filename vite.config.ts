6d8aa9d7dc38a94d1d5bee5c894f7206db323a88
Pasha <pashaie@gmail.com>
2023-01-11 09:19:31 +0330

    deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pashautils/',
   plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
 })
