b8ad6261854b78b3d0cc580665737085fe80642e
Pasha <pashaie@gmail.com>
2023-01-18 09:53:20 +0330

    qr code - responsive

--- src/Qr.tsx
+++ src/Qr.tsx
@@ -1,4 +1,4 @@
-import { Col, Divider, QRCode, Row } from "antd";
+import { Col, Divider, QRCode, Row, Space } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import React, { useState } from "react";
 
@@ -6,15 +6,15 @@ export default function Qr() {
   const [val, setVal] = useState("qr");
   return (
     <div>
-      <Row>
-        <Col span={12} offset={6}>
+      <Row justify="center">
+        <Col xs={{ span: 24 }} md={{ span: 18, offset: 3 }}>
           <TextArea
             placeholder="text or url"
             value={val}
             onChange={(e) => setVal(e.target.value)}
           />
-          <Divider></Divider>
-          <QRCode size={128} value={val} />
+          <Divider>QR Code</Divider>
+          <QRCode value={val} />
         </Col>
       </Row>
     </div>
